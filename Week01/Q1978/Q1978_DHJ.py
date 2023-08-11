1723c8552a6e982aea1bcc8de38d54cdfbc097de
Designated-Hitter <dosadola@gmail.com>
2023-08-11 20:37:13 +0900

    [ADD - DHJ] Q1978 풀이

--- Week01/Q1978/Q1978_DHJ.py
+++ Week01/Q1978/Q1978_DHJ.py
@@ -2,16 +2,20 @@ N = int(input())
 num_list = list(input().split())
 num_list = list(map(int,num_list)) #숫자 배열
 count = 0
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
 for i in num_list:
-    for j in range(2, i):
-        if i % j != 0:
-            pass
-        else:
-            break
-    count += 1
+    if is_prime(i):
+        count += 1
 
 print(count)
-print(1 % 2)
     
 
                 
