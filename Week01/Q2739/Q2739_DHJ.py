f293c55ba3f7fb8e3d0ba1e68c3ed02a0cb131d0
JEONG DONG-HWAN <91516840+Designated-Hitter@users.noreply.github.com>
2023-08-11 10:40:40 +0900

    Add files via upload

---
+++ Week01/Q2739/Q2739_DHJ.py
@@ -0,0 +1,10 @@
+N = int(input()) #파이썬에서 템플릿 리터럴 사용하기
+print(f'{N} * 1 = {N * 1}') #문법은 f'{} 어쩌고 저쩌고 {}'
+print(f'{N} * 2 = {N * 2}')
+print(f'{N} * 3 = {N * 3}')
+print(f'{N} * 4 = {N * 4}')
+print(f'{N} * 5 = {N * 5}')
+print(f'{N} * 6 = {N * 6}')
+print(f'{N} * 7 = {N * 7}')
+print(f'{N} * 8 = {N * 8}')
+print(f'{N} * 9 = {N * 9}')
