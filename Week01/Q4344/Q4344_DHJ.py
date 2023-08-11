a3d6e6547d38a412fd5286bb51fff4d3734d13a3
Designated-Hitter <dosadola@gmail.com>
2023-08-11 19:55:53 +0900

    [ADD - DHJ]Q9202 풀이

--- Week01/Q4344/Q4344_DHJ.py
+++ Week01/Q4344/Q4344_DHJ.py
@@ -5,9 +5,11 @@ for i in range(C):
     grade_list.pop(0)
     grade_list = list(map(int, grade_list)) #정수 타입 배열로 변경
     average = sum(grade_list) / num #평균값
-    print(average)
-    for i in grade_list:
-        count = 0
-        if i > average:
+    count = 0
+    for grade in grade_list:
+        if grade > average:
             count += 1
-    print(count)
+    percentage = count / num * 100
+    print(f'{percentage:.3f}%') #소숫점 아래 3자리까지 표시
+
+    #파이썬에서 소수 몇 자리까지 표시할 것인가 형식
