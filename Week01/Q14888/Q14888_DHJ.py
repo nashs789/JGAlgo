89e3ab0451eeae5f2319a0804daf70cae9ecab43
Designated-Hitter <dosadola@gmail.com>
2023-08-15 19:43:44 +0900

    [ADD - DHJ]Q14888 solve

--- Week01/Q14888/Q14888_DHJ.py
+++ Week01/Q14888/Q14888_DHJ.py
@@ -13,15 +13,29 @@ for _ in range(multiply):
     operator_list.append('multiply')
 for _ in range(divide):
     operator_list.append('divide')
-print(operator_list)
+
 possible_list = [0] * (len(num_list) - 1)
-possible_list_list = []
+answer_list = []
 def perm(n, k):
     if n == k:
-        print(possible_list)
-        temp = possible_list.copy() #여기서 temp에 현재의 possible_list 값을 복사하지 않으면 list_list에는 마지막에 생성된 list가 여러번 들어가게 된다
-        possible_list_list.append(temp)
+        #가능한 리스트를 구하고 바로 결과를 계산하기
+        temp_value = num_list[0]
+        for i in range(0, len(possible_list)):
+            if possible_list[i] == 'plus':
+                temp_value = temp_value + num_list[i + 1]
+            elif possible_list[i] == 'minus':
+                temp_value = temp_value - num_list[i + 1]
+            elif possible_list[i] == 'multiply':
+                temp_value = temp_value * num_list[i + 1]
+            else:
+                if temp_value < 0:
+                    temp_value = temp_value * -1
+                    temp_value = (temp_value // num_list[i + 1]) * -1
+                else:
+                    temp_value = temp_value // num_list[i + 1]
 
+        answer_list.append(temp_value)
+            
     else:
         for i in range(0, n):
             if visited[i] : continue
@@ -32,41 +46,43 @@ def perm(n, k):
     
 perm(len(operator_list), 0)
 
-print(possible_list_list)
-value_list = []
-for sub_list in possible_list_list:
-    print('1')
-    print(sub_list)
-    for operator in sub_list:
-        print('2')
-        print(operator)
-        temp_result = num_list[0]
-        print('3')
-        print(num_list)
-        if operator == 'plus':
-            temp_result = temp_result + num_list[1]
-            num_list.pop(0)
-            num_list.pop(1)
-            print(temp_result)
-        elif operator == 'minus':
-            temp_result = temp_result - num_list[1]
-            num_list.pop(0)
-            num_list.pop(1)
-            print(temp_result)
-        elif operator == 'multiply':
-            temp_result = temp_result * num_list[1]
-            num_list.pop(0)
-            num_list.pop(1)
-            print(temp_result)
-        elif operator == 'divide':
-            if temp_result < 0:
-                temp_result = temp_result * -1
-            temp_result = (temp_result // num_list[1]) * -1
-            num_list.pop(0)
-            num_list.pop(1)
-            print(temp_result)
+print(max(answer_list))
+print(min(answer_list))
+
+# print(possible_list_list)
+# value_list = []
+# for sub_list in possible_list_list:
+#     print('1')
+#     print(sub_list)
+#     for operator in sub_list:
+#         print('2')
+#         print(operator)
+#         temp_result = num_list[0]
+#         print('3')
+#         print(num_list)
+#         if operator == 'plus':
+#             temp_result = temp_result + num_list[1]
+#             num_list.pop(0)
+#             num_list.pop(1)
+#             print(temp_result)
+#         elif operator == 'minus':
+#             temp_result = temp_result - num_list[1]
+#             num_list.pop(0)
+#             num_list.pop(1)
+#             print(temp_result)
+#         elif operator == 'multiply':
+#             temp_result = temp_result * num_list[1]
+#             num_list.pop(0)
+#             num_list.pop(1)
+#             print(temp_result)
+#         elif operator == 'divide':
+#             if temp_result < 0:
+#                 temp_result = temp_result * -1
+#             temp_result = (temp_result // num_list[1]) * -1
+#             num_list.pop(0)
+#             num_list.pop(1)
+#             print(temp_result)
 
 
-        value_list.append(temp_result)
+#         value_list.append(temp_result)
 
-print(value_list)
