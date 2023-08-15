855b87294e0415ec9c6c1ebe763e88a89ee14214
hjlee5701 <hjlee5701@gmail.com>
2023-08-15 16:04:18 +0900

    [ feat: HJ ] 차이를 최대로

---
+++ Week01/Q10819/Q10819_LHJ.py
@@ -0,0 +1,28 @@
+import sys
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+
+def backtrack(depth):
+    global totalNum
+    if depth == N:
+        totalNum = sum(abs(combLst[i] - combLst[i + 1]) for i in range(N-1))
+        result.append(totalNum)
+        return
+    for i in range(N):
+        if visited[i]:
+            continue
+        
+        combLst.append(A[i])
+        visited[i] = True
+        backtrack(depth + 1)
+        visited[i] = False
+        combLst.pop()
+        
+        
+result = []
+combLst = []
+visited = [False] * N
+
+backtrack(0)
+
+print(max(result))
