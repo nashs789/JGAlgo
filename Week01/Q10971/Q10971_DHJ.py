605121a79a99d1864cc530a903cde4521c43d89e
Designated-Hitter <dosadola@gmail.com>
2023-08-15 16:05:25 +0900

    [ADD - DHJ] 문제 푸는 중

--- Week01/Q10971/Q10971_DHJ.py
+++ Week01/Q10971/Q10971_DHJ.py
@@ -6,27 +6,32 @@ for i in range(N):
     #행렬을 담은 이중 리스트
     matrix_list.append(matrix)
 
-print(matrix_list)
-
-city_list = []
-for i in range(N):
-    city_list.append(i)
-
-print(city_list)
-
-possible_route = [0] * N
-possible_route_list = []
+city_list = list(range(N))
 visited = [0] * N
+possible_route = [0] * N
+minimum = sys.maxsize
 
 #경로 구하기 순열
 def perm(n, k):
 
     if n == k:
-        new_list = []
-        for i in possible_route:
-            new_list.append(i)
-        possible_route_list.append(new_list)
+        circle_list = []
+        total_cost = 0
 
+        #실현가능한 모든 도시 경로 
+        circle_list = possible_route
+        #순회루트이므로 처음에 왔던 도시 추가
+        circle_list.append(possible_route[0])
+
+        global minimum
+        for i in range(0, len(circle_list) - 1):
+
+            cost = matrix_list[circle_list[i]][circle_list[i+1]]
+            if cost == 0: 
+                continue
+            total_cost = total_cost + cost
+
+        minimum = min(minimum, total_cost)
 
     else:
         for i in range(0, n):
@@ -38,26 +43,4 @@ def perm(n, k):
 
 perm(N, 0)
 
-#실현가능한 모든 도시 경로 
-possible_route_list.append(possible_route)
-
-#순회루트이므로 처음에 왔던 도시 추가
-for route in possible_route_list:
-    route.append(route[0])
-
-total_cost = []
-total_cost_list = []
-for route in possible_route_list:
-    for i in range(0,len(route) - 1):
-        cost = matrix_list[route[i]][route[i+1]]
-
-        if cost == 0:
-          pass
-
-        total_cost.append(cost)
-
-    sum_total_cost = sum(total_cost)
-    total_cost_list.append(sum_total_cost)
-    total_cost.clear()
-
-print(min(total_cost_list))
+print(minimum)
