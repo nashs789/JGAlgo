f31c299c388a65dcbe4b432f239551ec30e5e374
DongPark987 <55174232+DongPark987@users.noreply.github.com>
2023-09-04 22:23:53 +0900

    부모 버그 수정

--- Week04/DongPark987/rbTree.c
+++ Week04/DongPark987/rbTree.c
@@ -11,8 +11,10 @@ rbtree *new_rbtree(void) {
 
 void rightTurn(node_t* now, rbtree* t, int mode);
 void leftTurn(node_t* now, rbtree* t, int mode);
-void colorSwap(node_t* cur); //인자 부모노드
-
+void colorSwap(node_t* cur); //인자 부모노드rbPrint
+int cnt;
+int cnt2;
+void rbPrint(node_t* root);
 
 void delete_node(node_t* N) {
 	if (N->left != NULL) {
@@ -39,38 +41,32 @@ int checkCase(node_t* now, rbtree* t) {
 		return 0;
 	//케이스1 좌선형
 	if (grand->right != NULL && grand->left != NULL && grand->right->color == RBTREE_RED && grand->left->color == RBTREE_RED) {
-		//printf("쌍빨강\n");
-
+		printf("쌍빨강\n");
 		colorSwap(parent,t);
 		t->root->color = RBTREE_BLACK;
 		return 5;
 	}
 	if (now == parent->left && parent == grand->left) {
-		//printf("좌선형\n");
-
+		printf("좌선형\n");
 		rightTurn(now, t, 0);
 		return 1;
 	}
 
 	if (now == parent->right && parent == grand->left) {
-		//printf("좌꺽형\n");
-
+		printf("좌꺽형\n");
 		leftTurn(now, t, 1);
 		rightTurn(now->left, t, 0);
 		return 2;
 	}
 	if (now == parent->right && parent == grand->right) {
-		//printf("우선형\n");
-
+		printf("우선형\n");
 		leftTurn(now, t, 0);
 		return 3;
 	}
 	if (now == parent->left && parent == grand->right) {
-		//printf("우꺽형\n");
-		
+		printf("우꺽형\n");
 		rightTurn(now, t, 1);
 		leftTurn(now->right, t, 0);
-
 		return 4;
 	}
 }
@@ -121,9 +117,9 @@ void rightTurn(node_t* now, rbtree *t, int mode) {
 	grand->parent = parent;
 	//조부의 좌측 => 부모의 우측
 	grand->left = parent->right;
-
+	if (parent->right != NULL)
+		parent->right->parent = grand;
 	//부모의 우측 => 조부
-	//printf("%d의 우측 = %d\n", parent->key, grand->key);
 	parent->right = grand;
 
 	//부모 조부 색상 스와핑
@@ -159,13 +155,15 @@ void leftTurn(node_t* now, rbtree* t, int mode) {
 		parent = now;
 		grand = now->parent;
 	}
-
+	printf("좌회전, 부모%d, 조부%d, 조조부\n", parent->key, grand->key);
 	//부모의 부모 => 조부의 부모
 	parent->parent = grand->parent;
 	//조부의 부모 => 부모
 	grand->parent = parent;
 	//조부의 우측 => 부모의 좌측
 	grand->right = parent->left;
+	if(parent->left!=NULL)
+		parent->left->parent = grand;
 
 	//부모의 좌측 => 조부
 	//printf("좌회전 %d의 우측 = %d\n", parent->key, grand->key);
@@ -202,10 +200,11 @@ node_t* rbtree_insert(rbtree* t, const key_t key) {
 	newNode->parent = NULL;
 	newNode->left = NULL;
 	newNode->right = NULL;
+	newNode->data = cnt2;
 
 	if (t->root == NULL) {
 		newNode->color = RBTREE_BLACK;
-		newNode->parent = NULL;
+		newNode->parent = t->root;
 		t->root = newNode;
 		return t->root;
 	}
@@ -219,6 +218,10 @@ node_t* rbtree_insert(rbtree* t, const key_t key) {
 			else {
 				newNode->parent = cur;
 				cur->left = newNode;
+				//printf("회전 전\n");
+				//cnt = 0;
+				//rbPrint(t);
+				//printf("\n");
 				checkCase(newNode,t);
 				return t->root;
 
@@ -229,8 +232,13 @@ node_t* rbtree_insert(rbtree* t, const key_t key) {
 				cur = cur->right;
 			}
 			else {
-				cur->right = newNode;
 				newNode->parent = cur;
+				cur->right = newNode;
+				//printf("회전 전\n");
+				//cnt = 0;
+				//rbPrint(t);
+				//printf("\n");
+
 				checkCase(newNode,t);
 				return t->root;
 
@@ -273,6 +281,27 @@ node_t *rbtree_max(const rbtree *t) {
 int rbtree_erase(rbtree* t, node_t* p) {
 	// TODO: implement erase
 
+	node_t* cur = p;
+
+	//우측 우선으로 자식이 있는지 확인
+	if (cur->right != NULL) {
+		cur = cur->right;
+		while (cur->left != NULL)
+			cur = cur->left;
+		//cur은 우측에서 가장 작은 값
+		p->key = cur->key;
+		if(cur->parent->right == cur)
+			cur->parent->right = cur->right;
+		else
+			cur->parent->left = cur->right;
+	}
+	else {
+		if (cur->parent->right == cur)
+			cur->parent->right = cur->left;
+		else
+			cur->parent->left = cur->left;
+	}
+	free(cur);
 	return 0;
 }
 
@@ -282,12 +311,11 @@ int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
 }
 
 
-int cnt;
 void rbPrint(node_t* root) {
 	cnt++;
 
 
-	printf("번호: %d, 출력: %d, 색상: %d", cnt, root->key, root->color);    // 루트노드 방문
+	printf("번호:%d cnt:%d  출력: %d, 색상: %d",cnt,root->data, root->key, root->color);    // 루트노드 방문
 	if (root->left == NULL) {
 		printf(" 좌자식: %d", -1);
 	}
@@ -296,12 +324,22 @@ void rbPrint(node_t* root) {
 
 	}
 	if (root->right == NULL) {
-		printf(" 우자식: % d\n", -1);
+		printf(" 우자식: %d", -1);
 	}
 	else {
-		printf(" 우자식: %d\n", root->right->key);
+		printf(" 우자식: %d", root->right->key);
 
 	}
+
+	if (root->parent == NULL) {
+		printf(" 부모: root\n");
+	}
+	else {
+		printf(" 부모: %d\n",root->parent->data);
+
+	}
+
+
 	if (root->left != NULL)
 		rbPrint(root->left);    // 왼쪽 서브트리
 	//cnt--;
@@ -314,23 +352,37 @@ void rbPrint(node_t* root) {
 
 void deleteOne(rbtree* p, int key) {
 	node_t* find = rbtree_find(p, key);
+	printf("파인드: %d\n", find->key);
 	rbtree_erase(p, find);
-
 }
 
 int main() {
 	rbtree* R = new_rbtree();
-	printf("%d", R->root);
 
 
 	int num[] = { 99,45,1000,8,32,22,843,4123,5345,6777,788,2,0,9 };
 	int num2[] = {1,10,25,9,-55,13,60};
+	//int num3[] = { 10,10,10,22,30,100,23,22,-199,22,22 };
+	int num3[] = { 1,2,3,4,5,6,7,8,9,10 };
+	//int num3[] = { 5,1,3 };
+	int num4[] = { 1,2,4,10,10,10,10,10,10 };
+	//int num4[] = { 10,10,10,10,1,12,13,10,10 };
 
-	for (int i = 0; i <= 6; i++) {
+	int size = 10;
+	cnt2 = 0;
+	for (int i = 0; i <size; i++) {
+		cnt2 = i;
 		printf("\n");
-		rbtree_insert(R, num2[i]);
+		printf("%d 입력\n", num3[i]);
+		rbtree_insert(R, num3[i]);
 		cnt = 0;
 		rbPrint(R->root);
 	}
+
+	deleteOne(R,6);
+	printf("\n");
+	cnt = 0;
+	rbPrint(R->root);
+	
 	return 0;
 }
