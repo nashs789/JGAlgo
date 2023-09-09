ab8f2601edceb3b7440b0d5fd8103cfb1091a440
LeeInBok <bbock1214@gmail.com>
2023-09-09 10:54:38 +0900

    [ Add - LIB ] Day - 2 문제 풀이 (해결 x)

---
+++ MorningTask/Day_2/Q2293_LIB.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n, k = map(int, input().split())
+    coins = []
+    dp = [0] * (k + 1)
+    dp[0] = 1
+
+    for _ in range(n):
+        coins.append(int(input()))
+
+    for coin in coins:
+        for idx in range(coin, k + 1):
+            if idx - coin > 0:
+                dp[idx] += dp[idx - coin]
+        print(dp)
+
+    print(dp)
