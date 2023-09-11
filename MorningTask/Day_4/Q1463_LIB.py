aa813596aae3f19b622e5a0a824e871836fd91be
LeeInBok <bbock1214@gmail.com>
2023-09-11 10:30:30 +0900

    [ Add & Upt - LIB ] 아침 과제 수행

--- MorningTask/Day_4/Q1463_LIB.py
+++ MorningTask/Day_4/Q1463_LIB.py
@@ -1,16 +1,17 @@
 import sys
-sys.setrecursionlimit(500 * 500)
+sys.setrecursionlimit(2000 * 1000)
 
 def sol(n, cnt):
-    if dp[n] <= cnt:
+    if dp[1] <= cnt or n < 1:
         return
-    
+
     dp[n] = cnt
     cnt = cnt + 1
 
     if n % 3 == 0:
         sol(n // 3, cnt)
-    elif n % 2 == 0:
+    
+    if n % 2 == 0:
         sol(n // 2, cnt)
     
     sol(n - 1, cnt)
