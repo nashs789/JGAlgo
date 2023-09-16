ae7fd0fc6cc2d6ca0b2a386375895503cf03cb15
tomoyo519 <tomoyo519@naver.com>
2023-09-16 10:38:01 +0900

    [ADD - CDH] 두용액 완료

--- MorningTask/Day_5/2470(2)-CDH.py
+++ MorningTask/Day_5/2470(2)-CDH.py
@@ -1,19 +1,30 @@
 import sys
+import math
 input = sys.stdin.readline;
 
 N = int(input());
-potions = [(map(int, input().split() )) for _ in range(N)]
+potions = list(map(int, input().split() ))
 
 # start ,end, middle, 종료조건
 # middle값을 우째야하노..
 potions.sort(); # [-99 -2 -1 4 98]
-start , end = 0, len(potions) -1 ; # 인덱스로 가야되나 ..
-result = 0;
-while(start < end): # 0,4
-    if(potions[start] + potions[end] < result): # 최소값이다.
-        result = potions[start] + potions[end]; # result 갱신.. 그 이후는요 ?'
-        start +=1;
-    elif(start + end > result):
-        end -=1;
+start , end = 0, N -1 ; # 인덱스로 가야되나 ..
+result = math.inf
+resultArr = []
 
-print(potions[start], potions[end]);        
+while(start < end): # 0,4
+    sumsum = potions[start] + potions[end];
+    if(abs(sumsum) < abs(result)):
+        result = sumsum;
+        resultArr = [potions[start], potions[end]];
+    if(result == 0):
+        result = sumsum;
+        resultArr = [potions[start], potions[end]];
+        break;
+        
+    if(sumsum < 0):
+        start +=1;
+    else:
+        end-=1
+
+print(resultArr[0], resultArr[1]);
