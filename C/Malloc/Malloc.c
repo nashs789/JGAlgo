2f9fabbc98773a94fb1f702bd53ee7c5f190ebf6
LeeInBok <bbock1214@gmail.com>
2023-09-21 03:38:10 +0900

    [ Upt - LIB ] 변경사항 저장

--- C/Malloc/Malloc.c
+++ C/Malloc/Malloc.c
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 
 int main(){
+    int *pList = NULL;
 
+    pList = malloc(sizeof(int) * 3);
+    
+    printf("%lu\n", sizeof(pList));
+    pList[0] = 10;
+    pList[1] = 20;
+    pList[2] = 30;
+    pList[4] = 40;
+
+    *(((char*)pList) + 13) = 'A';
+    printf("%d\n", pList[4]);
+
+    free(pList);
 
     return 0;
 } 
